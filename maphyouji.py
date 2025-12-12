5fa2848b8e811ddf1f3637dc0aad9be46fbf2163
tanaka-ren0629 <2301092@s.asojuku.ac.jp>
2025-12-12 21:50:31 +0900

    Update maphyouji.py

--- maphyouji.py
+++ maphyouji.py
@@ -1,6 +1,7 @@
 import webbrowser#Python 標準ライブラリ に含まれるモジュールで、既定のウェブブラウザを起動して指定したURLを開くために使います。 import Flask, render_template
 import cv2
-webbrowser.open("clickable_map.html")
+webbrowser.open("japan_bike_rental_map.html")
+
 
 
 
